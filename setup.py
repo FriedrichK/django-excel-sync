3f85d237933064a7b44ead2b1c41e7ee2b7a2336
FriedrichK <friedrich@cartogami.com>
2014-04-03 17:52:35 +0200

    updated to actually include the package structure

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
 import os
-from setuptools import setup
-
+from setuptools import setup, find_packages
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 VERSION = __import__('excel_sync').__version__
+
+EXCLUDE_FROM_PACKAGES = []
+
+REQUIRES = [
+    'Django==1.6.2',
+    'mock==1.0.1',
+    'mockito==0.5.2',
+    'xlrd==0.9.2'
+]
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-excel-sync",
     version=VERSION,
-    packages=['excel_sync'],
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     include_package_data=True,
+    install_requires=REQUIRES,
     license='BSD',
     description='A Django app to synchronize data between Excel files and Django Models',
     long_description=README,
