58f70aaa44ff19afdf72595d5fdb1ebf8935ba03
FriedrichK <friedrich@cartogami.com>
2014-04-14 21:19:53 +0200

    fixing error where same table is filled again and again for each model processed by syncdb

--- excel_sync/db/models/__init__.py
+++ excel_sync/db/models/__init__.py
@@ -3,12 +3,13 @@ from django.dispatch import receiver
 
 from excel_sync.db.models.mixins import SpreadsheetModelMixin
 
+models_processed = []
+
 
 @receiver(post_syncdb)
 def import_spreadsheet_data(sender, **kwargs):
-    if not sender.__name__ == 'excel_sync.models':
-        return
-    for klass in kwargs["created_models"]:
+    models_left_to_process = get_models_left_to_process(kwargs["created_models"])
+    for klass in models_left_to_process:
         if(is_spreadsheet_model(klass)):
             klass.import_spreadsheet_data(klass)
 
@@ -17,3 +18,13 @@ def is_spreadsheet_model(klass):
     if hasattr(klass._meta, 'spreadsheet_source'):
         return True
     return False
+
+
+def get_models_left_to_process(models):
+    global models_processed
+    models_left_to_process = []
+    for model in models:
+        if not model in models_processed:
+            models_left_to_process.append(model)
+    models_processed += models_left_to_process
+    return models_left_to_process
